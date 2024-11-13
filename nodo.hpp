48029b259033dd22f4e5284f7d7f28d23ddce6b8
Samuuffdzz <118897018+Samuuffdzz@users.noreply.github.com>
2024-11-13 16:55:10 +0100

    Update nodo.hpp

--- nodo.hpp
+++ nodo.hpp
@@ -14,13 +14,13 @@ private:
     string pal;
     list<Pagina*> l;
     Nodo *lft, *rgt;
-    int alturaLft() { return lft ? lft->h : -1; }
-    int alturaRgt() { return rgt ? rgt->h : -1; }
-    void actAltura() { h = max(alturaLft(), alturaRgt()) + 1; }
 public:
     Nodo(string p, Pagina* pg) : h(0), pal(p), l(pg ? list<Pagina*>({pg}) : list<Pagina*>()), lft(nullptr), rgt(nullptr) {}
     Nodo(string p) : Nodo(p, nullptr) {}
     Nodo() : Nodo("", nullptr) {}
+    int alturaLft() { return lft ? lft->h : -1; }
+    int alturaRgt() { return rgt ? rgt->h : -1; }
+    void actAltura() { h = max(alturaLft(), alturaRgt()) + 1; }
     ~Nodo() { delete lft; delete rgt; }
     void insertar(const string& pal, Pagina* pag);
     list<Pagina*> buscar(const string& pal);
