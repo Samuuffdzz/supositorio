cfd51ca982205b17dc3fd40f91996523fcd92ba6
Samuuffdzz <118897018+Samuuffdzz@users.noreply.github.com>
2024-11-13 16:54:46 +0100

    Update arbol.cpp

--- arbol.cpp
+++ arbol.cpp
@@ -7,6 +7,9 @@ void Arbol::insertar(string pal, Pagina* pag) {
     }
 
     raiz->insertar(pal, pag);
+    int h_lft = raiz->alturaLft(), h_rgt = raiz->alturaRgt();
+    if (h_lft > h_rgt + 1) raiz = raiz->RSI();
+    if (h_rgt > h_lft + 1) raiz = raiz->RSD();
 }
 
 list<Pagina*> Arbol::buscar(string pal) {
